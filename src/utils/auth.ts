14a73427d5038613c49c092941b5504e26adc7fa
Lea Baumgart <lea@abmgrt.dev>
2020-12-20 23:25:27 +0100

    fuck

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,8 +3,10 @@ const axios = require("axios").default;
 const auth = async (req) => {
 	if (!req.headers.authorization) return;
 	try {
-		const sessionRequest = await axios.post("https://micro.alles.cx/api/me", {
-			token: req.headers.authorization,
+		const sessionRequest = await axios.get("https://micro.alles.cx/api/me", {
+			headers: {
+				Authorization: req.headers.authorization,
+			},
 		});
 		return sessionRequest.data;
 	} catch (err) {
